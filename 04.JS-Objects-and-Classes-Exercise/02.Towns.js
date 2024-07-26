01d7d6146c55ea8415a3c404a109db2a65c63a5c
SamuelLazarov <samuel.lazarov@gmail.com>
2024-07-26 10:55:46 +0300

    Added variants

--- 04.JS-Objects-and-Classes-Exercise/02.Towns.js
+++ 04.JS-Objects-and-Classes-Exercise/02.Towns.js
@@ -17,3 +17,23 @@ function storeTowns(input) {
 }
 
 storeTowns(['Sofia | 42.696552 | 23.32601','Beijing | 39.913818 | 116.363625']);
+
+function storeTownsVariant(input) {
+    const towns = input.reduce((acc, cur) => {
+        const [town, latitude, longitude] = cur.split(" | ");
+
+        acc.push({
+            town,
+            latitude: Number(latitude).toFixed(2),
+            longitude: Number(longitude).toFixed(2),
+        });
+
+        return acc;
+    }, []);
+
+    towns.forEach((town) => {
+        console.log(town);
+    });
+}
+
+storeTownsVariant(['Sofia | 42.696552 | 23.32601','Beijing | 39.913818 | 116.363625']);
