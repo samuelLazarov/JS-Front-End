83979494d1ca04df153899c7759747fe4fd07293
SamuelLazarov <samuel.lazarov@gmail.com>
2024-07-26 14:11:26 +0300

    solve functions added

--- 04.JS-Objects-and-Classes-Exercise/03.Store-provision.js
+++ 04.JS-Objects-and-Classes-Exercise/03.Store-provision.js
@@ -1,10 +1,10 @@
 function storeProvision(inStockProducts, orderedProducts) {
     function generateObjectData(inputArr) {
-        return new Array(inputArr.length/2).fill({}).reduce((acc, curr, i) => {
-            const product = inputArr[i + 1];
+        return new Array(inputArr.length / 2).fill({}).reduce((acc, curr, i) => {
+            const product = inputArr[i + i];
             const quantity = Number(inputArr[i + i + 1]);
 
-            if(!curr[product]){
+            if (!curr[product]){
                 curr[product] = 0;
             }
 
@@ -30,3 +30,11 @@ function storeProvision(inStockProducts, orderedProducts) {
         console.log(`${key} -> ${value}`);
     });
 }
+
+storeProvision([
+    'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
+    ],
+    [
+    'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+    ]
+    )
