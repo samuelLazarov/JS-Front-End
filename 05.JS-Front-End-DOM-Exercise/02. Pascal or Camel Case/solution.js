fad9f2ff71434e8eaa0dd955f1111ba1162d90a5
SamuelLazarov <samuel.lazarov@gmail.com>
2024-07-28 14:31:07 +0300

    functions added

--- 05.JS-Front-End-DOM-Exercise/02. Pascal or Camel Case/solution.js
+++ 05.JS-Front-End-DOM-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  const validNamingConventions = ["Camel Case", "Pascal Case"];
+
+  const textInputEl = document.querySelector("#text");
+  const namingConventionsEl = document.querySelector("#naming-convention");
+  
+  const resultEl = document.querySelector(".result-container #result");
+
+  if (!validNamingConventions.includes(namingConventionsEl.value)) {
+    resultEl.textContent = "Error!";
+    return;
+  }
+
+  const pascalCaseText = textInputEl.value
+    .toLowerCase()
+    .split(" ")
+    .map((x) => x[0].toUpperCase().concat(x.slice(1)))
+    .join("");
+
+  resultEl.textContent =
+    namingConventionsEl.value === validNamingConventions[0]
+    ? pascalCaseText[0].toLowerCase().concat(pascalCaseText.slice(1)) 
+    : pascalCaseText;
 }
