40138774d209a8b99b89dd9a721bb581717aa117
SamuelLazarov <samuel.lazarov@gmail.com>
2024-08-01 15:18:06 +0300

    DOM & Event exercise

--- 06.JS-Front-End-DOM-Events-Exercise/06.JS-Front-End-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
+++ 06.JS-Front-End-DOM-Events-Exercise/06.JS-Front-End-DOM-Events-Exercise-Resources/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonElements = document.querySelectorAll("div.profile button");
+
+    function showMoreClickHandler(event) {
+        const button = event.target;
+        const [, unlockInputEl] = button.parentElement.querySelectorAll("input");
+
+        if (!unlockInputEl.checked) {
+            return;
+        }
+
+        const hiddenDataEl = button.parentElement.querySelector("div");
+
+        if (button.textContent === "Show more") {
+            hiddenDataEl.style.display = "block";
+            button.textContent = "Hide it";
+        } else {
+            hiddenDataEl.style.display = "none";
+            button.textContent = "Show more";
+        }
+    }
+
+    buttonElements.forEach((buttonElement) =>
+        buttonElement.addEventListener("click", showMoreClickHandler)
+    );
 }
