df09734b726bea1e20b920604e6e2ea288053c09
SamuelLazarov <samuel.lazarov@gmail.com>
2024-08-01 15:25:21 +0300

    DOM & Event exercise

--- 06.JS-Front-End-DOM-Events-Exercise/06.JS-Front-End-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ 06.JS-Front-End-DOM-Events-Exercise/06.JS-Front-End-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,24 @@
 function addItem() {
-    console.log('TODO:...');
+    const [textInputEl, valueInputEl] = document.querySelectorAll("input");
+
+    function createOptionElement(text, value) {
+        const option = document.createElement("option");
+        option.textContent = text;
+        option.value = value;
+
+        return option;
+        
+    }
+
+    function clearInputs() {
+        textInputEl.value = "";
+        valueInputEl.value = "";
+    }
+
+    const optionEl = createOptionElement(textInputEl.value, valueInputEl.value);
+
+    document.getElementById("menu").appendChild(optionEl);
+
+    clearInputs();
+
 }
