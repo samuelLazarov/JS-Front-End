21271605e1d5ae47003fbbb2159d74955d6c443e
SamuelLazarov <samuel.lazarov@gmail.com>
2024-08-02 19:25:09 +0300

    DOM & Event exercise

--- 06.JS-Front-End-DOM-Events-Exercise/06.JS-Front-End-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ 06.JS-Front-End-DOM-Events-Exercise/06.JS-Front-End-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,27 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeBtnEl, decodeBtnEl] = document.querySelectorAll("div button");
+    const [encodeTextAreaEl, decodeTextAreaEl] = document.querySelectorAll("div textarea");
+
+    function encodeOrDecodeMessage(text, asciiDiff) {
+        return text
+        .split("")
+        .map((char) => {
+            const currentAsciiValue = char.charCodeAt(0);
+            return String.fromCharCode(currentAsciiValue + asciiDiff);
+        })
+        .join("");
+    }
+
+    function encodeMessageHandler() {
+        decodeTextAreaEl.value = encodeOrDecodeMessage(encodeTextAreaEl.value, 1);
+        encodeTextAreaEl.value = "";
+    }
+
+    function decodeMessageHandler() {
+        decodeTextAreaEl.value = encodeOrDecodeMessage(decodeTextAreaEl.value, -1);
+    }
+
+    encodeBtnEl.addEventListener("click", encodeMessageHandler);
+    decodeBtnEl.addEventListener("click", decodeMessageHandler);
+
 }
