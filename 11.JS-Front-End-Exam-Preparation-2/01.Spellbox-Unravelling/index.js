0e74d2480e3ff791ba8cb639388dc3d6e12cbe36
SamuelLazarov <samuel.lazarov@gmail.com>
2024-08-13 21:54:26 +0300

    More stuff

---
+++ 11.JS-Front-End-Exam-Preparation-2/01.Spellbox-Unravelling/index.js
@@ -0,0 +1,42 @@
+((input) => {
+    const [mysteriousSpell, ...commands] = input;
+
+    const spell = commands.reduce((currentSpell, currentCommand) => {
+        if (currentCommand === "RemoveEven") {
+            const result = currentSpell
+                .split("")
+                .filter((_, index) => index % 2 === 0)
+                .join("");
+            console.log(result);
+            return result;
+        }
+
+        if (currentCommand.startsWith("TakePart!")) {
+            const [, from, to] = currentCommand.split("!");
+            const result = currentSpell.slice(Number(from),Number(to));
+            console.log(result);
+            return result;
+        }
+
+        if (currentCommand.startsWith("Reverse!")) {
+            const [, substring] = currentCommand.split("!");
+
+            if (!currentSpell.includes(substring)) {
+                console.log("Error");
+                return currentSpell;
+            }
+
+            const reversedValue = substring.split("").reverse().join("");
+            const result = currentSpell.replace(substring, "").concat(reversedValue);
+            console.log(result);
+            return result;
+        }
+
+        return currentSpell;
+    }, mysteriousSpell);
+
+    console.log("The concealed spell is:", spell);
+})
+
+(["asAsl2adkda2mdaczsa", "RemoveEven", "TakePart!1!9", "Reverse!maz", "End"])
+    
---
+++ 11.JS-Front-End-Exam-Preparation-2/02.Adopt-Me_Resources/app.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", solve);
+
+function solve() {
+    const adoptButtomElement = document.getElementById("adobt-btn");
+
+    const typeInputElement = document.getElementById("type");
+    const ageInputElement = document.getElementById("age");
+    const genderInputElement = document.getElementById("gender");
+
+    const adoptionInfoListElements = document.getElementById("adoption-info");
+
+    function createAnimalListItem () {
+      const liElement = document.createElement("li");
+      const articleElement = document.createElement("article");
+
+      const typeParagraphElement = document.createElement("p");
+      typeInputElement.textContent = typeInputElement.value;
+      articleElement.appendChild(typeParagraphElement);
+
+      const genderParagraphElement = document.createElement("p");
+      genderParagraphElement.textContent = genderInputElement.value;
+      articleElement.appendChild(genderParagraphElement);
+
+      const ageParagraphElement = document.createElement("p");
+      ageParagraphElement.textContent = ageInputElement.value;
+      articleElement.appendChild(ageParagraphElement);
+
+      liElement.appendChild(articleElement);
+      
+      adoptionInfoListElements.appendChild(liElement);
+    }
+
+    function onAdoptClickHandler(event) {
+      event.preventDefault();
+      createAnimalListItem();
+
+      
+
+    }
+
+
+
+    adoptButtomElement.addEventListener("click", onAdoptClickHandler);
+  }
+  
