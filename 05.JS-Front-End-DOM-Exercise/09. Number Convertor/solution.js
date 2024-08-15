26da3a424532d3724843904e01e90f1a83c776a0
SamuelLazarov <samuel.lazarov@gmail.com>
2024-08-15 14:28:42 +0300

    More stuff

--- 05.JS-Front-End-DOM-Exercise/09. Number Convertor/solution.js
+++ 05.JS-Front-End-DOM-Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,23 @@
 function solve() {
+    const dropDown = document.getElementById('selectMenuTo');
+    let binary = document.createElement('option');
+    binary.text = 'Binary';
+    binary.value = 'binary';
+    let hex = document.createElement('option');
+    hex.text = 'Hexadecimal';
+    hex.value = 'hexadecimal';
+    dropDown.appendChild(binary);
+    dropDown.appendChild(hex);
 
-    //TODO...
+    const button = document.getElementsByTagName('button')[0];
+    button.addEventListener('click', () => {
+        const number = Number(document.getElementById('input').value);
+        const convertTo = document.getElementById('selectMenuTo').value;
+        const outputTarget = document.getElementById('result');
+        if (convertTo === 'binary') {
+            outputTarget.value = number.toString(2);
+        } else if (convertTo === 'hexadecimal') {
+            outputTarget.value = number.toString(16).toUpperCase();
+        }
+    })
 }
